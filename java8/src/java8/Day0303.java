64b1019363b9a75617c475327b340230684a3410
choieunbi <choieunbi.dev@gmail.com>
2020-03-16 13:05:59 +0900

    3일차: 변수의 범위

---
+++ java8/src/java8/Day0303.java
@@ -0,0 +1,21 @@
+package java8;
+
+import java.awt.print.Printable;
+
+public class Day0303 {
+	public static void main(String[] args) {
+		int number1 = 100;	//지역변수
+		System.out.println(number1);
+		
+		{	//중괄호 블럭
+			System.out.println(number1);
+			int number2 = 200;	//블럭변수
+			System.out.println(number2);
+		
+		}
+		
+//		System.out.println(number1);
+//		System.out.println(number2);	외부의 것은 내부에서 쓸수 있지만 
+//										내부의 것은 외부에서 사용할 수 없다
+	}
+}
