ff6373c6e9e55b9c45ab9162b79e297a7d6db191
choieunbi <choieunbi.dev@gmail.com>
2020-03-17 15:21:55 +0900

    4일차 : 실습문제 3

---
+++ java8/src/java8/Day0406.java
@@ -0,0 +1,17 @@
+package java8;
+
+public class Day0406 {
+
+	public static void main(String[] args) {
+		int intyear = 1988;
+		int intmonth = 05;
+		int intdate = 27;
+		double ablweight = 63.2;
+		double ablheight = 176.5;
+		System.out.println("积斥岿老 : " + intyear + "." + intmonth +"." + intdate);
+		System.out.println("个公霸   : " + ablweight );
+		System.out.println("虐       : " + ablheight );
+		
+	}
+
+}
