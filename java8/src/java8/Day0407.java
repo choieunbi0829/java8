15136fda5091be08634fa2f6845ffc57c87e0abb
choieunbi <choieunbi.dev@gmail.com>
2020-03-17 15:22:09 +0900

    4일차 : 실습문제 4

---
+++ java8/src/java8/Day0407.java
@@ -0,0 +1,21 @@
+package java8;
+
+public class Day0407 {
+	public static void main(String[] args) {
+		int a = 8;
+		int b = 5;
+		
+		int res1 = a + b;
+		int res2 = a - b;
+		int res3 = a * b;
+		int res4 = a / b;
+		int res5 = a % b;
+		
+		System.out.println("a+b의 값은" + res1);
+		System.out.println("a-b의 값은" + res2);
+		System.out.println("a*b의 값은" + res3);
+		System.out.println("a/b의 값은" + res4);
+		System.out.println("a/b의 나머지값은" + res5);
+		
+		}
+}
