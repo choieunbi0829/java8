638ad7a46cf59c85a15faa74413d22909ade3be2
choieunbi <choieunbi.dev@gmail.com>
2020-03-19 14:46:24 +0900

    5일차 : ELSE IF구문

---
+++ java8/src/java8/Day0503.java
@@ -0,0 +1,34 @@
+package java8;
+
+import java.util.Scanner;
+
+public class Day0503 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("당신의 영어 점수를 입력해 주세요(0~100): ");
+		int score = sc.nextInt();
+		String pass = "";
+		
+		if(score >=90) {
+			pass = "A학점";
+			} else if(score>=80) {
+				pass = "B학점";
+			} else if(score>=70) {
+				pass = "C학점";
+			} else if(score>=60) {
+				pass = "D학점";
+			} else  {
+				pass = "F학점";
+			}	
+	
+		System.out.println("시험성적은 " + pass + "입니다.");
+		sc.close();
+	}
+}
+
+/*90점 이상은 A학점
+ * 90점 미만 80점 이상은 B학점
+ * 80점 미만 70점 이상은 C학점
+ * 70점 미만 60점 이상은 D이상
+ * 60점 미만은 F학점
+ */
