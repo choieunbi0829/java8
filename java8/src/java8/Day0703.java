bcc8202664d53b28aa616aa3678d95bf0ac4e71b
choieunbi <choieunbi.dev@gmail.com>
2020-03-23 09:42:19 +0900

    7일차 : 예제 3

---
+++ java8/src/java8/Day0703.java
@@ -0,0 +1,14 @@
+package java8;
+
+public class Day0703 {
+	public static void main(String[] args) {
+		
+		int sum=0;
+		
+		for(int i=1; i<=10; i++) {
+			sum += i;
+		}
+		System.out.println("1부터 10까지의 수의 합계는 " + sum + "입니다.");
+		
+	}
+}
