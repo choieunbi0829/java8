a077013fd23dc617ff98ea0015734c1678553b9c
choieunbi <choieunbi.dev@gmail.com>
2020-04-01 15:08:09 +0900

    14일 : 연습 1

---
+++ java8/src/day14/Airplane.java
@@ -0,0 +1,13 @@
+package day14;
+
+public class Airplane {
+	void land() {
+		System.out.println("착륙합니다.");
+	}
+	void fly() {
+		System.out.println("일반비행합니다.");
+	}
+	void takeoff() {
+		System.out.println("이륙합니다");
+	}
+}
