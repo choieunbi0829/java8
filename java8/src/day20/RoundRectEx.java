0a9e0540958f56e412dd7cdce0640cc3cc708915
choieunbi <choieunbi.dev@gmail.com>
2020-04-09 15:58:57 +0900

    20일차 : 5

---
+++ java8/src/day20/RoundRectEx.java
@@ -0,0 +1,16 @@
+package day20;
+
+import javax.swing.JFrame;
+
+public class RoundRectEx {
+	public static void main(String[] args) {
+		JFrame f = new JFrame("사각형 그리기");
+		RoundRect p = new RoundRect();
+		
+		f.setSize(500, 500);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.add(p);
+		f.setResizable(false);
+		f.setVisible(true);
+	}
+}
