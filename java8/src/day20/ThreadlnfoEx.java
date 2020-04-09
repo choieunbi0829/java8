41c81c35f619b155c1c4c8bdb025c3900c941335
choieunbi <choieunbi.dev@gmail.com>
2020-04-09 13:39:17 +0900

    20일차 : 4

---
+++ java8/src/day20/ThreadlnfoEx.java
@@ -0,0 +1,21 @@
+package day20;
+
+import java.util.Map;
+import java.util.Set;
+
+public class ThreadlnfoEx {
+	public static void main(String[] args) {
+		AutoSaveThread autoSaveThread = new AutoSaveThread();		//데몬 스레드의 AutoSaveThread 클래스
+		autoSaveThread.setName("AutoSaveThread");
+		autoSaveThread.setDaemon(true);
+		autoSaveThread.start();			//start전에 setDaemon이 들어가야지 바뀜
+		
+		Map<Thread, StackTraceElement[]> map = Thread.getAllStackTraces();
+		Set<Thread> threads = map.keySet();
+		for(Thread thread : threads) {
+			System.out.println("Name: " + thread.getName() + ((thread.isDaemon())?"(데몬)": "(주)"));
+			System.out.println("\t" + "소속그룹: " + thread.getThreadGroup().getName());
+			System.out.println();
+		}
+	}
+}
